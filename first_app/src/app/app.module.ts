2bb694e7201eaaa81d968f997d84dd7ffc339d77
RaphaelBecker <raphael.becker.private@outlook.de>
2022-08-24 17:45:39 +0200

    28

--- first_app/src/app/app.module.ts
+++ first_app/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
--- first_app/src/app/warning-alert/warning-alert.component.ts
+++ first_app/src/app/warning-alert/warning-alert.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from "@angular/core";
 
 @Component ({
     selector: 'app-warning-alert',
-    templateUrl: './warning-alert.component.html'
+    templateUrl: './warning-alert.component.html',
+    styles: [`p { 
+        padding: 20px;
+        background-color: pink;
+        border: 1px;
+        color: red; }`]
 })
-export class WarningAlertConponent implements OnInit {
+export class WarningAlertComponent implements OnInit {
 
     constructor() { }
   
